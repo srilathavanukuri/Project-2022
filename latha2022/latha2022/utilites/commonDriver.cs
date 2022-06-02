a2e9d78ba8e038d798172e06ee04c66c2a7da6ab
SRILATHA <srilathareddy203@gmail.com>
2022-06-02 17:34:30 +1200

    update TM-Tests

---
+++ latha2022/latha2022/utilites/commonDriver.cs
@@ -0,0 +1,14 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace latha2022.utilites
+{
+    public class commonDriver
+    {
+        public static IWebDriver driver;
+    }
+}
